a8a04535a351252eb874cdabfed9042cae58017a
Michael Chase <themikechase@gmail.com>
2024-07-08 06:59:48 -0700

    beginning time-series forecast

--- ocean_color.py
+++ ocean_color.py
@@ -25,3 +25,56 @@ System_band = first_image.get('system:band_names')
 print('system:band_names', System_band.getInfo())
 
 
+# Data Filtering
+selected_bands = 'chlor_a'
+sY=2003
+sM=1
+sD=1
+eY=2021
+eM=9
+eD=28
+start_date = ee.Date.fromYMD(sY, sM, sD)
+end_date = ee.Date.fromYMD(eY, eM, eD)
+print(str(start_date))
+
+
+count = ChlaDS.size()
+valTest = count.getInfo()
+print('Image count : ', str(valTest)+'\n')
+
+# Compute average Chl-a for the whole period
+DecadalMeanChla = ChlaDS.reduce(ee.Reducer.mean())
+
+# Visualize calculated mean
+val = cm.palettes.Spectral
+palette=val.default
+
+Vis = {
+  'min': 0,
+  'max': 0.5,
+  'palette': palette
+}
+
+Map.setCenter(-30,14,5)
+Map.addLayer(DecadalMeanChla, Vis, 'MeanChl-a')
+Map.addLayer(ChlaDS.first(),Vis,'FirstImage')
+Map
+
+# Start time-series forecast
+years = ee.List.sequence(sY, eY)
+months = ee.List.sequence(sM, eM)
+
+def getMonthlyMean(iy):
+
+
+
+
+
+
+
+
+
+
+
+
+
